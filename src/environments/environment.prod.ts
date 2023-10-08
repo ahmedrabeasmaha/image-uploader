b4246c8049e4904258aca556d7e37e5e207fd34f
Ahmed Rabea Smaha <35260162+ahmedrabeasmaha@users.noreply.github.com>
2023-10-08 14:19:03 +0300

    update api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://image-uploader-api.netlify.com/api/',
-<<<<<<< HEAD
-  imageUrl: 'https://image-uploader-api.netlify.com/api/images/'
-=======
   imageUrl: 'https://image-uploader-api.netlify.com/api/images/',
->>>>>>> b390bd8 (update api link)
 };
